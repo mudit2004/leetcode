c20a7c4962469932ce754ef9164df8739c329d1d
Mudit Golchha <golchhamudit2203@gmail.com>
2024-09-19 22:37:50 +0530

    Time: 31 ms (90.48%) | Memory: 16.9 MB (8.08%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -3,24 +3,15 @@ from typing import List
 
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        
-        # Use an LRU cache to memoize results automatically
         @lru_cache(None)
         def compute(expr):
-            # If the expression is a pure number, return it as a list
             if expr.isdigit():
                 return [int(expr)]
-            
             res = []
-            
-            # Iterate through the expression to find operators
             for i, char in enumerate(expr):
-                if char in '+-*':  # If the character is an operator
-                    # Recursively compute left and right sides
+                if char in '+-*': 
                     left = compute(expr[:i])
                     right = compute(expr[i+1:])
-                    
-                    # Compute results based on the operator
                     for l in left:
                         for r in right:
                             if char == '+':
@@ -29,8 +20,5 @@ class Solution:
                                 res.append(l - r)
                             elif char == '*':
                                 res.append(l * r)
-            
             return res
-        
-        # Call the helper function on the full expression
         return compute(expression)
