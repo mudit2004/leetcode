cfdde44601e359c291ec61a3fab98524f49c83e6
Mudit Golchha <golchhamudit2203@gmail.com>
2025-02-03 16:19:51 -0500

    Time: 3 ms (51.34%) | Memory: 17.9 MB (34.55%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,25 +1,24 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+
         max_length = 0
 
-        for i in range(len(nums)):
-            curr_length = 1
-            for j in range(i+1,len(nums)):
-                if nums[j] > nums[j-1] :
-                    curr_length += 1
-                else:
-                    break
-            max_length = max(max_length , curr_length)
+        for i in range(n):
+            for increasing in [True, False]:  # Check both increasing and decreasing
+                curr_length = 0
+                for j in range(i, n):
+                    if j > i: #if not the first element of the sub-array
+                        if increasing and nums[j] > nums[j-1]:
+                            curr_length += 1
+                        elif not increasing and nums[j] < nums[j-1]:
+                            curr_length += 1
+                        else:
+                            break #break if the monotonicity is broken
+                    else: #if the first element
+                        curr_length = 1
+                max_length = max(max_length, curr_length)
 
-        for i in range(len(nums)):
-            curr_length = 1
-            for j in range(i+1,len(nums)):
-                if nums[j] < nums[j-1] :
-                    curr_length += 1
-                else:
-                    break
-            max_length = max(max_length , curr_length)
-        
-        
         return max_length
-        
